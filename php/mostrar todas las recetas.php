f5ceb4db8fd7f2a59c817189c1e68799e6586de4
MatiasEP <matias.ezequiel.piedrabuena@gmail.com>
2020-10-25 00:33:40 -0300

    update mostrar todas las recetas
    Ahora muestra todas las recetas en forma descendente, desde las mas nuevas hasta las mas viejas. Ademas, solo se visualizaran las recetas con atributo 'visible':true

--- php/mostrar todas las recetas.php
+++ php/mostrar todas las recetas.php
@@ -23,25 +23,28 @@
                     'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
                 
                 $filter = [];
-                $options = [];
-                $query = new MongoDB\Driver\Query($filter);
+                $options = ['sort' =>['_id'=>-1],];
+                $query = new MongoDB\Driver\Query($filter,$options);
                 $rows = $client->executeQuery("proyecto.recetas", $query); // $mongo contains the connection object to MongoDB
                 foreach($rows as $r)
                 {
-                    echo("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>");
-                    echo("<div class='panel panel-primary' >");    
-                    echo("<div class='panel-heading'>".$r->titulo."</div>");
-                    echo("<div class='panel-body' >");        
-                    echo("<img src=".$r->imagen." class='thumbnail' alt='no tenia fotos de culo''>");
-                    echo("<div class='panel-footer' >Tipo: ");
-                    foreach($r->tipo as $tipo)
-                    {       
-                        echo(" ".$tipo." ");
+                    if($r->visible)
+                    {                        
+                        echo("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>");
+                        echo("<div class='panel panel-primary' >");    
+                        echo("<div class='panel-heading'>".$r->titulo."</div>");
+                        echo("<div class='panel-body' >");        
+                        echo("<img src=".$r->imagen." class='thumbnail' alt='no tenia fotos de culo''>");
+                        echo("<div class='panel-footer' >Tipo: ");
+                        foreach($r->tipo as $tipo)
+                        {       
+                            echo(" ".$tipo." ");
+                        }
+                        echo("</div>");
+                        echo("</div>");
+                        echo("</div>");
+                        echo("</div>");
                     }
-                    echo("</div>");
-                    echo("</div>");
-                    echo("</div>");
-                    echo("</div>");
                 }
             ?>
         </div>
