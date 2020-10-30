6b6ed4bb541e6757b9f724802047a88273819c0f
MatiasEP <matias.ezequiel.piedrabuena@gmail.com>
2020-10-30 00:23:55 -0300

    mostrar todas las recetas
    ahora mostrar todas las recetas esta separado entre la pagina principal, un php para traer las recetas de la base de datos y pasarlos a un json, un js que toma ese json y muestra dinamicamente los panels de las recetas en la pagina

---
+++ scripts/mostrar todas las recetas.js
@@ -0,0 +1,46 @@
+$(document).ready(inicio)
+
+function tipos(aTipos)
+{
+    let array = [];
+    {for(let j=0; j<aTipos.length;j++)
+    {
+        array.push(" "+aTipos[j]+" ");
+    }}
+    return array;
+}
+
+function mostrarTodasLasRecetas()
+{
+    let request = $.ajax(
+    {
+        method: "POST",
+        url: "../json/mostrar todas las recetas.json"
+    });
+        $("#main").empty();
+        request.done(function(data) {            
+        for(let i = 0; i<data.length;i++)
+        {
+            $("#main").append("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>"+
+            "<div class='panel panel-primary' >"+   
+            "<div class='panel-heading'>"+data[i]["titulo"]+"</div>"+
+            "<div class='panel-body' >"+       
+            "<img src="+data[i]["imagen"]+" class='thumbnail' alt='no tenia fotos de culo'>"+
+            "<div class='panel-footer' >Tipo: "+tipos(data[i]["tipo"])+
+            
+            "</div>"+
+            "</div>"+
+            "</div>"+
+            "</div>");
+        }
+        
+    })
+    request.fail(function() {
+    alert("Algo salió mal");
+    });
+}
+
+function inicio()
+{
+    mostrarTodasLasRecetas();
+}
