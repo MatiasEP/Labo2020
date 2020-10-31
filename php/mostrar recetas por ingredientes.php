e00b4586804437ae1cfa536659a06f893c5758d2
MatiasEP <matias.ezequiel.piedrabuena@gmail.com>
2020-10-30 21:31:00 -0300

    mostrar receta por ingredientes - a terminar
    mostrar por ingredientes ahora esta separado en 2 php, uno para la vista y otro para la funcionalidad de las querys, tambien esta separado en un js para tomar datos del json creado por php y mostrar los panels de las recetas en forma dinamica.
    
    TO-DO: mejorar la query para que tome mas ingredientes, actualmente solo filtra por 1 ingrediente

---
+++ php/mostrar recetas por ingredientes.php
@@ -0,0 +1,34 @@
+<?php
+    use \MongoDB\Driver\BulkWrite;
+    use \MongoDB\Driver\Query;
+    use \MongoDB\Driver\ReadPreference;
+    $ingrediente = isset($_POST["ingrediente"])?$_POST["ingrediente"]:'';
+    $client = new MongoDB\Driver\Manager(sprintf(
+        'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
+    $ingrediente = isset($_POST["ingrediente"])?$_POST["ingrediente"]:'';
+    $ingrediente = "pepino";//provisorio para test
+    if($ingrediente !='')
+    {            
+        $filter = ["ingredientes.descripcion"=>$ingrediente];
+        $options = ['sort' =>['_id'=>-1],];
+        $query = new MongoDB\Driver\Query($filter,$options);
+        $rows = $client->executeQuery("proyecto.recetas", $query); // $mongo contains the connection object to MongoDB    
+        $file = '../json/mostrar recetas por ingredientes.json';
+        if(file_exists($file))
+        {        
+            unlink($file);
+        }
+        //file_put_contents($file,"[",FILE_APPEND | LOCK_EX);
+        $array = array();
+        foreach ($rows as $row) 
+        {
+            $json_string = json_encode($row);
+            array_push($array, $json_string);
+        }
+        $array = implode(',',$array);
+        file_put_contents($file, "[",FILE_APPEND | LOCK_EX);
+        file_put_contents($file, $array,FILE_APPEND | LOCK_EX);
+        file_put_contents($file, "]",FILE_APPEND | LOCK_EX);
+    }
+    
+?>
