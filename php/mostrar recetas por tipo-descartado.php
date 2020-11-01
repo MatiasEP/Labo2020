072e93a367d636bba34e91c9fc7ada5ee4d9e913
MatiasEP <matias.ezequiel.piedrabuena@gmail.com>
2020-10-31 21:17:48 -0300

    mostrar recetas por tipo
    ahora estan separados en archivos php, js y json

---
+++ php/mostrar recetas por tipo-descartado.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../estilos/mostrar todas las recetas.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+
+            <?php
+                use \MongoDB\Driver\BulkWrite;
+                use \MongoDB\Driver\Query;
+                use \MongoDB\Driver\ReadPreference;
+                $tipo = isset($_POST["tipo"])?$_POST["tipo"]:'';
+                $client = new MongoDB\Driver\Manager(sprintf(
+                    'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
+                if($id != '')
+                {
+                    $filter = ["_idCreador"=>$tipo];
+                    $options = ['sort' =>['_id'=>-1],];
+                    $query = new MongoDB\Driver\Query($filter,$options);
+                    $rows = $client->executeQuery("proyecto.recetas", $query); // $mongo contains the connection object to MongoDB
+                    foreach($rows as $r)
+                    {
+                        if($r->visible and $r->activado)
+                        {                        
+                            echo("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>");
+                            echo("<div class='panel panel-primary' >");    
+                            echo("<div class='panel-heading'>".$r->titulo."</div>");
+                            echo("<div class='panel-body' >");        
+                            echo("<img src=".$r->imagen." class='thumbnail' alt='no tenia fotos de culo''>");
+                            echo("<div class='panel-footer' >Tipo: ");
+                            foreach($r->tipo as $tipo)
+                            {       
+                                echo(" ".$tipo." ");
+                            }
+                            echo("</div>");
+                            echo("</div>");
+                            echo("</div>");
+                            echo("</div>");
+                        }
+                    }
+                }
+                
+            ?>
+        </div>
+    </div>
+</body>
+</html>
--- php/mostrar recetas por tipo.php
+++ php/mostrar recetas por tipo.php
@@ -1,57 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../estilos/mostrar todas las recetas.css" type="text/css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-
-            <?php
-                use \MongoDB\Driver\BulkWrite;
-                use \MongoDB\Driver\Query;
-                use \MongoDB\Driver\ReadPreference;
-                $tipo = isset($_POST["tipo"])?$_POST["tipo"]:'';
-                $client = new MongoDB\Driver\Manager(sprintf(
-                    'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
-                if($id != '')
-                {
-                    $filter = ["_idCreador"=>$tipo];
-                    $options = ['sort' =>['_id'=>-1],];
-                    $query = new MongoDB\Driver\Query($filter,$options);
-                    $rows = $client->executeQuery("proyecto.recetas", $query); // $mongo contains the connection object to MongoDB
-                    foreach($rows as $r)
-                    {
-                        if($r->visible and $r->activado)
-                        {                        
-                            echo("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>");
-                            echo("<div class='panel panel-primary' >");    
-                            echo("<div class='panel-heading'>".$r->titulo."</div>");
-                            echo("<div class='panel-body' >");        
-                            echo("<img src=".$r->imagen." class='thumbnail' alt='no tenia fotos de culo''>");
-                            echo("<div class='panel-footer' >Tipo: ");
-                            foreach($r->tipo as $tipo)
-                            {       
-                                echo(" ".$tipo." ");
-                            }
-                            echo("</div>");
-                            echo("</div>");
-                            echo("</div>");
-                            echo("</div>");
-                        }
-                    }
-                }
-                
-            ?>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+    use \MongoDB\Driver\BulkWrite;
+    use \MongoDB\Driver\Query;
+    use \MongoDB\Driver\ReadPreference;
+    $ingrediente = isset($_POST["ingrediente"])?$_POST["ingrediente"]:'';
+    $client = new MongoDB\Driver\Manager(sprintf(
+        'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
+    $ingrediente = isset($_POST["tipo"])?$_POST["tipo"]:'';
+    $ingrediente = "culo";//provisorio para test
+    if($ingrediente !='')
+    {            
+        $filter = ["tipo"=>$ingrediente];
+        $options = ['sort' =>['_id'=>-1],];
+        $query = new MongoDB\Driver\Query($filter,$options);
+        $rows = $client->executeQuery("proyecto.recetas", $query); // $mongo contains the connection object to MongoDB    
+        $file = '../json/mostrar recetas por tipo.json';
+        if(file_exists($file))
+        {        
+            unlink($file);
+        }
+        //file_put_contents($file,"[",FILE_APPEND | LOCK_EX);
+        $array = array();
+        foreach ($rows as $row) 
+        {
+            $json_string = json_encode($row);
+            array_push($array, $json_string);
+        }
+        $array = implode(',',$array);
+        file_put_contents($file, "[",FILE_APPEND | LOCK_EX);
+        file_put_contents($file, $array,FILE_APPEND | LOCK_EX);
+        file_put_contents($file, "]",FILE_APPEND | LOCK_EX);
+    }
+    
+?>
