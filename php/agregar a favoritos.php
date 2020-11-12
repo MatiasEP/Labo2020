1d95aefc1fb57be961996e826288dc7e570a9ed4
MatiasEP <matias.ezequiel.piedrabuena@gmail.com>
2020-11-11 23:21:33 -0300

    Update agregar a favoritos.php
    agrega una receta a la lista de favoritos de un usuario, si esa receta no esta en la lista ya

--- php/agregar a favoritos.php
+++ php/agregar a favoritos.php
@@ -6,12 +6,25 @@
         'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
     $idUsuario = isset($_POST["idUsuario"])?$_POST["idUsuario"]:'';
     $idReceta = isset($_POST["idReceta"])?$_POST["idReceta"]:'';
+    $idReceta = "5fa58577bd0d000028004567";
+    $idUsuario = "5fa1f37a25700000930007ed"; //temporal para test
     if($idUsuario != '' and $idReceta !='')
-    {
-        $query = new BulkWrite();
-        $query->update(['_idUsuario' => $idUsuario],
-        ['$addset' => ['Recetas' => $idReceta]]);
-        $client->executeBulkWrite("proyecto.favoritos",$query);
+    {        
+        $idReceta = new MongoDB\BSON\ObjectId($idReceta);        
+        $idUsuario = new MongoDB\BSON\ObjectId($idUsuario);
+        $filter = ["_idUsuario"=>$idUsuario, "Recetas"=>$idReceta];
+        $options = ['sort' =>['_id'=>-1],];
+        $query = new MongoDB\Driver\Query($filter,$options);
+        $rows = $client->executeQuery("proyecto.favoritos", $query);
+        $res = $rows->toArray();
+        $count = count($res);
+        if($count == 0)
+        {            
+            $query2 = new BulkWrite();
+            $query2->update(['_idUsuario' => $idUsuario],
+            ['$push' => ['Recetas' => $idReceta]]);
+            $client->executeBulkWrite("proyecto.favoritos",$query2);
+        }
     }
    
 ?>
