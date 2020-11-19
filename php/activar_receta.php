0f04aa1963d3e7f35de847898b5288e6f6a0d773
MatiasEP <matias.ezequiel.piedrabuena@gmail.com>
2020-11-18 22:20:12 -0300

    activar y ocultar receta
    activar_receta: el usuario activa una receta que el haya creado, para que los demas puedan verla.
    
    ocultar_receta: un administrador oculta una receta

---
+++ php/activar_receta.php
@@ -0,0 +1,30 @@
+
+<?php
+    use \MongoDB\Driver\BulkWrite;
+    use \MongoDB\Driver\Query;
+    $client = new MongoDB\Driver\Manager(sprintf(
+        'mongodb+srv://labo2020:labo2020@cluster0.wvxvt.mongodb.net/proyecto?retryWrites=true&w=majority'));
+    $idUsuario = isset($_POST["idUsuario"])?$_POST["idUsuario"]:'';
+    $idReceta = isset($_POST["idReceta"])?$_POST["idReceta"]:'';
+    $idReceta = "5fa58577bd0d000028004567";
+    $idUsuario = "5fa1f37a25700000930007ed"; //temporal para test
+    if($idUsuario != '' and $idReceta !='')
+    {        
+        $idReceta = new MongoDB\BSON\ObjectId($idReceta);        
+        $idUsuario = new MongoDB\BSON\ObjectId($idUsuario);
+        $filter = [ "_id"=>$idReceta,"_idCreador"=>$idUsuario,"activado"=>false,"visible"=>true];
+        $options = ['sort' =>['_id'=>-1],];
+        $query = new MongoDB\Driver\Query($filter,$options);
+        $rows = $client->executeQuery("proyecto.recetas", $query);
+        $res = $rows->toArray();
+        $count = count($res);
+        if($count != 0)
+        {            
+            $query2 = new BulkWrite();
+            $query2->update(["_id"=>$idReceta,'_idCreador' => $idUsuario,"activado"=>false,"visible"=>true],
+            ['$set' => ['activado' => true]]);
+            $client->executeBulkWrite("proyecto.recetas",$query2);
+        }
+    }
+   
+?>
